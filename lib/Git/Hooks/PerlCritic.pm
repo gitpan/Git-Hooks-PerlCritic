94f0d3022a413b68217ff583c8f2e30fd94d42e1
Mikko Koivunalho <mikkoi@cpan.org>
2014-11-23 08:29:25 +0000

    Import of MIKKOI/Git-Hooks-PerlCritic-0.001001.tar.gz from CPAN.
    gitpan-cpan-distribution: Git-Hooks-PerlCritic
    gitpan-cpan-version:      0.001001
    gitpan-cpan-path:         MIKKOI/Git-Hooks-PerlCritic-0.001001.tar.gz
    gitpan-cpan-author:       MIKKOI
    gitpan-cpan-maturity:     released

--- lib/Git/Hooks/PerlCritic.pm
+++ lib/Git/Hooks/PerlCritic.pm
@@ -3,11 +3,13 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '0.001000'; # VERSION
+our $VERSION = '0.001001'; # VERSION
 
 use Carp;
 use Module::Load 'load';
 use Git::Hooks;
+
+(my $CFG = __PACKAGE__) =~ s/.*::/githooks./msx;
 
 sub _changed {
 	my $git = shift;
@@ -21,26 +23,30 @@ sub _changed {
 }
 
 sub _set_critic {
+   my ($git) = @_;
+   my $pc_rc_filename = $git->get_config($CFG => 'profile');
+
 	load 'Perl::Critic';
 	load 'Perl::Critic::Violation';
 	load 'Perl::Critic::Utils';
 
-	my $pc = Perl::Critic->new;
+	my $pc = Perl::Critic->new('-profile' => $pc_rc_filename//q{});
 	my $verbosity = $pc->config->verbose;
 
 	# set the format to be a comment
-	my $fmt = Perl::Critic::Utils::verbosity_to_format( $verbosity );
-	Perl::Critic::Violation::set_format( "# $fmt" );
+	my $format = Perl::Critic::Utils::verbosity_to_format( $verbosity );
+	Perl::Critic::Violation::set_format( "# $format" );
 
 	return $pc;
 }
 
 sub _check_violations {
+   my $git = shift;
 	my $files = shift;
 
 	my @violations;
-	foreach my $file ( @$files ) {
-		state $critic = _set_critic;
+	foreach my $file ( @{$files} ) {
+		state $critic = _set_critic($git);
 
 		@violations = $critic->critique( $file );
 	}
@@ -52,9 +58,9 @@ PREPARE_COMMIT_MSG {
 	my ( $git, $commit_msg_file ) = @_;
 
 	my $changed    = _changed( $git );
-	my $violations = _check_violations( $changed );
+	my $violations = _check_violations( $git, $changed );
 
-	if ( @$violations ) {
+	if ( @{$violations} ) {
 		my $pcf = 'Path::Class::File'; load $pcf;
 		my $file     = $pcf->new( $commit_msg_file );
 		my $contents = $file->slurp;
@@ -70,13 +76,13 @@ PRE_COMMIT {
 	my $git = shift;
 
 	my $changed    = _changed( $git );
-	my $violations = _check_violations( $changed );
+	my $violations = _check_violations( $git, $changed );
 
-	if ( @$violations ) {
-		print @$violations;
+	if ( @{$violations} ) {
+		print @{$violations};
 		# . operator causes the array ref to give count, otherwise it would
 		# stringify
-		croak '# please fix ' . @$violations . ' perl critic errors before committing';
+		croak '# please fix ' . @{$violations} . ' perl critic errors before committing';
 	}
 };
 
@@ -94,13 +100,13 @@ Git::Hooks::PerlCritic - Perl Critic hooks for git
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
-Allows you to utilize L<Perl::Critic> via
+Allows you to utilize L<Perl::Critic|Perl::Critic> via
 L<git hooks|http://www.kernel.org/pub/software/scm/git/docs/githooks.html>
-using the L<Git::Hooks> framework.
+using the L<Git::Hooks|Git::Hooks> framework.
 
 First setup L<git-hooks.pl|Git::Hooks/"USAGE">
 
@@ -110,28 +116,35 @@ Then you should choose to use only one of the available hooks.
 
 =head2 pre-commit
 
-	ln -s git-hooks.pl pre-commit
-	git config --add githooks.pre-commit PerlCritic
+	ln -s git-hooks.pl .git/hooks/pre-commit
+	git config --add githooks.plugin PerlCritic
 
-This hook will prevent a commit that doesn't pass L<Perl::Critic> from being
+This hook will prevent a commit that doesn't pass L<Perl::Critic|Perl::Critic> from being
 committed.
 
 =head2 prepare-commit-msg
 
-	ln -s git-hooks.pl prepare-commit-msg
-	git config --add githooks.preprare-commit-msg PerlCritic
+	ln -s git-hooks.pl .git/hooks/prepare-commit-msg
+	git config --add githooks.plugin PerlCritic
 
 This hook will simply append commented out critic warnings to the commit
-message, so you may review them before committing. Note: requires
-L<Path::Class::File>.
+message, so you may review them before committing.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+Option I<profile> to define a perlcriticrc file. E.g.:
+
+	git config --add githooks.perlcritic.profile subdir/.perlcriticrc
 
 =head1 SEE ALSO
 
 =over
 
-=item L<Git::Hooks>
+=item L<Git::Hooks|Git::Hooks>
 
-=item L<Perl::Critic>
+=item L<Perl::Critic|Perl::Critic>
+
+=item Alternative way to use Perl::Critic with Git::Hooks package: L<Git::Hooks::CheckFile|Git::Hooks::CheckFile/"CONFIGURATION">.
 
 =back
 
@@ -145,13 +158,23 @@ When submitting a bug or request, please include a test-file or a
 patch to an existing test-file that illustrates the bug or desired
 feature.
 
-=head1 AUTHOR
+=head1 AUTHORS
+
+=over 4
+
+=item *
 
 Caleb Cushing <xenoterracide@gmail.com>
 
+=item *
+
+Mikko Koivunalho <mikkoi@cpan.org>
+
+=back
+
 =head1 COPYRIGHT AND LICENSE
 
-This software is Copyright (c) 2013 by L<HostGator.com|http://hostgator.com>.
+This software is Copyright (c) 2014 by L<HostGator.com|http://hostgator.com>.
 
 This is free software, licensed under:
 
